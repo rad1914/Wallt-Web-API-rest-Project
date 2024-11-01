885355cae4236818ef29fe0443a32964f379c1bf
Sr RAD <51271703+rad1914@users.noreply.github.com>
2024-11-01 07:29:07 -0600

    Delete buttonMessage.js

--- buttonMessage.js
+++
@@ -1,8 +0,0 @@
-// buttonMessage.js
-
-import { sendMessage } from './messageHandler.js';
-
-export function sendButtonMessage(message) {
-    document.getElementById('messageInput').value = message;
-    sendMessage({ type: 'click' });
-}
